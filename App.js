5e1c1625dfba02ac265049af8a17643bb7a171d0
Matheus Da Costa <sousam150@gmail.com>
2020-11-10 22:27:33 -0300

    Tela de Login adcionada

--- App.js
+++ App.js
@@ -1,118 +1,20 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React from 'react';
 
-type Props = {};
-export default class App extends Component<Props> {
-  constructor(props) {
-    super(props);
-    this.state = { altura: 0, massa: 0, resultado: 0, resultadoText: '' };
-    this.calcular = this.calcular.bind(this);
-  }
+import Imc from './src/pages/imc';
+import Login from './src/login';
 
-  calcular() {
-    let imc = this.state.massa / (this.state.altura * this.state.altura);
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-    let s = this.state;
-    s.resultado = imc;
+const Stack = createStackNavigator();
 
-    // < 16 - Magreza Grave
-    // 16 a < 17 - Magreza Moderada
-    // 17 a < 18,5 - Magreza Leve
-    // 18,5 a < 25 - Saudável
-    // 25 a < 30 - Sobrepeso
-    // 30 a < 35 - Obesidade Grau 1
-    // 35 a < 40 - Obesidade Grau 2 (Grave)
-    // > 40- Obesidade Grau 3 (Mórbida, enterro em caixa d'água)
+const App = () => (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Imc" component={Imc} />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
 
-    if (s.resultado < 16) {
-      s.resultadoText = 'Magreza Grave';
-    } else if (s.resultado < 17) {
-      s.resultadoText = 'Magreza Moderada';
-    } else if (s.resultado < 18.5) {
-      s.resultadoText = 'Magreza Leve';
-    } else if (s.resultado < 25) {
-      s.resultadoText = 'Saudável';
-    } else if (s.resultado < 30) {
-      s.resultadoText = 'Sobrepeso';
-    } else if (s.resultado < 35) {
-      s.resultadoText = 'Obesidade Grau 1';
-    } else if (s.resultado < 40) {
-      s.resultadoText = 'Obesidade Grau 2';
-    }
-
-    this.setState(s);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.entradas}>
-            
-          <TextInput
-            placeholder="Massa"
-            keyboardType="numeric"
-            style={styles.input}
-            onChangeText={(massa) => {
-              this.setState({ massa });
-            }}
-          />
-          <TextInput
-            placeholder="Altura"
-            keyboardType="numeric"
-            style={styles.input}
-            onChangeText={(altura) => {
-              this.setState({ altura });
-            }}
-          />
-        </View>        
-        <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
-        <Text style={[styles.resultado, { fontSize: 35 }]}>
-          {this.state.resultadoText}
-        </Text>
-        <TouchableOpacity style={styles.buttom} onPress={this.calcular}>
-          <Text style={styles.buttomText}>Calcular</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  entradas: {
-    flexDirection: 'row',
-  },
-  input: {
-    height: 80,
-    textAlign: 'center',
-    width: '50%',
-    marginTop: 24,
-    color: 'black',
-  },
-  buttom: {
-    backgroundColor: '#57e1ee',
-  },
-  buttomText: {
-    alignSelf: 'center',
-    padding: 10,
-    margin: 5,
-    fontSize: 25,
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  resultado: {
-    alignSelf: 'center',
-    color: 'black',
-    fontSize: 65,
-    padding: 15,
-  },
-});
+export default App;
---
+++ src/login.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View,
+        KeyboardAvoidingView, 
+        Image, 
+        TextInput, 
+        TouchableOpacity, 
+        Text,
+        StyleSheet 
+    } from 'react-native';
+
+export default function Login({ navigation }) {
+    return (
+        <KeyboardAvoidingView style={styles.background}>
+            <View style={styles.containerLogo}>
+                <Image
+                source={require('../assets/img/icon.png')}
+                />
+            </View>
+
+            <View style={styles.container}>
+                <TextInput 
+                style={styles.input}
+                placeholder="Username"
+                autoCorrect={false}
+                onChangeText={()=> {}}
+                />
+
+                <TextInput
+                style={styles.input}
+                placeholder="Senha"
+                autoCorrect={false}
+                onChangeText={()=> {}}
+                />
+
+                <TouchableOpacity 
+                    style={styles.btnSubmit}
+                    onPress={() => navigation.navigate('Imc')}>
+                    <Text style={styles.submitText}>Entrar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnRegister}>
+                    <Text>Criar conta gratuita</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    background:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    containerLogo: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    container: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        paddingBottom: 50
+    },
+    input: {
+        backgroundColor: '#FFF',
+        width: '90%',
+        marginBottom: 15,
+        color: '#222',
+        fontSize: 17,
+        borderRadius: 7,
+        padding: 10,
+    },
+    btnSubmit: {
+        backgroundColor: '#87cefa',
+        width: '90%',
+        height: 45,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 7
+    }, 
+    submitText:{
+        fontSize: 18
+    },
+    btnRegister: {
+      marginTop: 10,  
+    },
+});
